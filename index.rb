8cf75abcb31d28da6a1fd9c3198a617650eb4c27
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-12-20 09:45:58 +1100

    added examples in readme

--- index.rb
+++ index.rb
@@ -1,1 +1,31 @@
-# IF THIS IS BLANK RIGHT NOW YOU DID IT RIGHT
+sydney_campus_location = " 3/118 Walker St, North Sydney, 2060"
+
+melbourne_campus_location = "8/120 Spencer Street, Melbourne Victoria"
+
+brisbane_campus_lcation = "60 Gloucester St, Spring Hill QLD 4000"
+
+sydney_students = [
+  {
+    name: "Alex",
+    age: 31,
+    id: "CAS011914",
+  },
+  {
+    name: "Luke",
+    age: 25,
+    id: "CAS011915",
+  },
+]
+
+melbstudents = [
+  {
+    name: "Drew",
+    age: 35,
+    id: "CAM011920",
+  },
+  {
+    name: "Sarah",
+    age: 20,
+    id: "CAM01125",
+  },
+]
